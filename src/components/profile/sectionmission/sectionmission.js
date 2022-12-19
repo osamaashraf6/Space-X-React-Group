9eb6d2c2de4b82516f105a9f651646ff81e35df1
momotsow <kgomotsomkansi@gmail.com>
2022-12-19 22:40:20 +0200

    Add missions styling

--- src/components/profile/sectionmission/sectionmission.js
+++ src/components/profile/sectionmission/sectionmission.js
@@ -4,10 +4,14 @@ import './sectionmission.css';
 function SectionMission() {
   return (
     <>
-      <div className="missions">
-        <div>SectionMission</div>
-        <div>SectionMission</div>
-        <div>SectionMission</div>
+      <div className="missions__container">
+        <h3 className="missions__headline">My Missions</h3>
+        <div className="missions">
+
+          <div className="missions__tab">SectionMission</div>
+          <div className="missions__tab">SectionMission</div>
+          <div className="missions__tab">SectionMission</div>
+        </div>
       </div>
     </>
   );
