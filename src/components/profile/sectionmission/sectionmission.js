569e7bb753a09ee967631cec8ca81f2783989d09
osamaashraf6 <osamaashraf668@gmail.com>
2022-12-19 22:05:06 +0200

    Add the two components for show the detected rockets and missions

---
+++ src/components/profile/sectionmission/sectionmission.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './sectionmission.css';
+
+function SectionMission() {
+  return (
+    <>
+      <div className="missions">
+        <div>SectionMission</div>
+        <div>SectionMission</div>
+        <div>SectionMission</div>
+      </div>
+    </>
+  );
+}
+
+export default SectionMission;
---
+++ src/components/profile/sectionrocket/sectionrocket.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './sectionrocket.css';
+
+function SectionRocket() {
+  return (
+    <>
+      <div className="rockets">
+        <div>SectionRockets</div>
+        <div>SectionRockets</div>
+        <div>SectionRockets</div>
+      </div>
+    </>
+  );
+}
+
+export default SectionRocket;
