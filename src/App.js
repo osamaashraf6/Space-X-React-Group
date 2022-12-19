780f432c08a30098d90aaaf83d84a4a1fd42c6e4
osamaashraf6 <osamaashraf668@gmail.com>
2022-12-19 20:13:00 +0200

    Add the react-router-dom in the project and add the navs for navigate in the other pages of the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/header/header';
+import Rocket from './pages/rocket';
+import Mission from './pages/mission';
+import Profile from './pages/profile';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      ds
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route index element={<Rocket />} />
+          <Route path="rocket" element={<Rocket />} />
+          <Route path="mission" element={<Mission />} />
+          <Route path="profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
