ef30a5224515f511bae71542eecdbf517ad3f54a
momotsow <kgomotsomkansi@gmail.com>
2022-12-19 21:17:28 +0200

    Add horizontal line on the navbar and import logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import React from 'react';
+import './header.css';
+import logo from '../../assets/planet2.png';
 
 function Header() {
   return (
@@ -8,11 +10,15 @@ function Header() {
         <div className="container">
           <div className="nav__container__parnav">
             <div className="nav__container__parnav__brand">
-              <a href="#logo">Books Store</a>
+              <a href="#logo" className="nav__container__parnav__brand__logo">
+                <img alt="logo-top" src={logo} className="nav__container__parnav__brand__logo__img" />
+                <h3>Space Traveler&#39;s Hub</h3>
+              </a>
             </div>
             <ul className="nav__container__parnav__ul">
               <li><NavLink to="/rocket">Rockets</NavLink></li>
               <li><NavLink to="/mission">mission</NavLink></li>
+              <hr />
               <li><NavLink to="/profile">profile</NavLink></li>
             </ul>
           </div>
