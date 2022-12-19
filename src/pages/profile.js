0e2a8e0b2f449ba55f28f059fa969504a9236845
osamaashraf6 <osamaashraf668@gmail.com>
2022-12-19 22:04:19 +0200

    Add the rockets and missions choices in the profile component

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import SecttionRocket from '../components/profile/sectionrocket/sectionrocket';
+import SecttionMission from '../components/profile/sectionmission/sectionmission';
 
 function Profile() {
   return (
-    <div>Profile</div>
+    <>
+      <SecttionRocket />
+      <SecttionMission />
+    </>
   );
 }
 
