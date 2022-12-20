73a6bd25457493aad93e24a09cba416f2ceb10c5
momotsow <kgomotsomkansi@gmail.com>
2022-12-20 11:11:16 +0200

    Import mission reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import missionsReducer from './missions/missions';
 
 const store = configureStore({
   reducer: {
-
+    mission: missionsReducer,
   },
 });
 export default store;
