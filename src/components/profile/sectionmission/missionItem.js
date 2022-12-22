1e5e32da4c04ea521855d9fb7a347aad5318f021
momotsow <kgomotsomkansi@gmail.com>
2022-12-22 17:18:48 +0200

    Added functionality to join a mission

--- src/components/profile/sectionmission/missionItem.js
+++ src/components/profile/sectionmission/missionItem.js
@@ -1,30 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { joinMission } from '../../../redux/missions/missions';
 
-const MissionList = ({ name, description }) => (
-  <tr className="row">
-    <td className="column name">{name}</td>
-    <td className="column description">{description}</td>
-    <td className="column status_column">
+const MissionList = ({
+  id, name, description, reserved,
+}) => {
+  const dispatch = useDispatch();
+  return (
+    <tr className="row">
+      <td className="column name">{name}</td>
+      <td className="column description">{description}</td>
+      <td className="column status_column">
+        {reserved
+        && <p className="ActiveStatus">Active Member</p>}
+        {!reserved
+        && <p className="status">NOT A MEMBER</p>}
+      </td>
+      <td className="column column_button">
 
-      <p className="status">NOT A MEMBER</p>
-    </td>
-    <td className="column column_button">
-
-      <button
-        type="button"
-        className="button"
-      >
-        Join Mission
-      </button>
-
-    </td>
-  </tr>
-);
+        {!reserved
+        && (
+        <button
+          type="button"
+          className="button"
+          onClick={() => dispatch(joinMission(id))}
+        >
+          Join Mission
+        </button>
+        )}
+      </td>
+    </tr>
+  );
+};
 
 MissionList.propTypes = {
   name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  reserved: PropTypes.bool.isRequired,
+
 };
 
 export default MissionList;
