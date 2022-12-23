ab16fb9476cce6c4074c7358b0d22fb5b14e9368
osamaashraf6 <osamaashraf668@gmail.com>
2022-12-23 17:36:16 +0200

    Change the text from the rocket section

--- src/components/profile/sectionrocket/sectionrocket.js
+++ src/components/profile/sectionrocket/sectionrocket.js
@@ -7,7 +7,7 @@ function SectionRocket() {
   return (
     <>
       <div className="rockets__container">
-        {rockets.length === 0 ? (<span>No</span>) : (<h3 className="rockets__headline">My Rockets</h3>)}
+        {rockets.length === 0 ? (<span>No Rockets here</span>) : (<h3 className="rockets__headline">My Rockets</h3>)}
         <div className="rockets">
           {rockets.map((rock) => (
             <div className="rockets__tab" key={rock.id}>{rock.rocketName}</div>
